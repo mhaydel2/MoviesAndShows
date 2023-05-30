c525aee9db18116a7ce5347a8339c0ae70a432b6
mhaydel2 <108110530+mhaydel2@users.noreply.github.com>
2023-05-30 00:12:34 -0500

    secured http

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=93522e73"
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=93522e73"
 
 
 const App = () => {
